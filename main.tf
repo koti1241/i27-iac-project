344193bdca8321a1e8046dc66d3ab624a4793933
koti <koteswararao1241@gmail.com>
2025-12-30 01:17:50 +0530

    vpc creation

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "google" {
+    project = "project-345d271b-412f-40a6-b53"
+    region = "us-central1"
+    #service account 
+    #credentials = file("key.json")
+}
---
+++ network.tf
@@ -0,0 +1,5 @@
+# creating vpc
+resource "google_compute_network" "i27-ecommerce-vpc" {
+    name = ""
+    
+}
