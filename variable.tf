ffabc0d7e9fcdd9b36ac1806c8ce55e3aeae7415
koti <koteswararao1241@gmail.com>
2026-01-01 02:14:36 +0530

    added keys and provisioners

--- variable.tf
+++ variable.tf
@@ -22,3 +22,13 @@ variable "instance" {
 
     }))
 }
+
+variable "VM-user" {
+    description = " user name of the vm"
+    type = string
+}
+
+variable "source_ranges" {
+    description = "enter the range of cidr range"
+    type = list(string)
+}
